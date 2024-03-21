e76ce17322327c029fa9eb44670ccbfe344a0dfe
giojano1 <giorgi.janelidze64@gmail.com>
2024-03-21 23:17:43 +0400

    filters
    filters done

--- js/main.js
+++ js/main.js
@@ -21,17 +21,38 @@ window.onclick = function (event) {
 };
 
 // FILTER
-function filterFunction() {
+function filterFunction1() {
   document.getElementById("filterDropdown").classList.toggle("show");
 }
 
 window.onclick = function (event) {
   if (
-    !event.target.matches(".filter-dropbtn") &&
-    !event.target.matches(".filter-dropbtn img") &&
-    !event.target.matches(".filter-dropbtn span")
+    !event.target.matches(".filter-dropbtn1") &&
+    !event.target.matches(".filter-dropbtn1 img") &&
+    !event.target.matches(".filter-dropbtn1 span")
   ) {
-    var dropdowns = document.getElementsByClassName("filter-dropdown-content");
+    var dropdowns = document.getElementsByClassName("filter-drop-1");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
+// FILTER
+function filterFunction2() {
+  document.getElementById("filterDropdown2").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+  if (
+    !event.target.matches(".filter-dropbtn2") &&
+    !event.target.matches(".filter-dropbtn2 img") &&
+    !event.target.matches(".filter-dropbtn2 span")
+  ) {
+    var dropdowns = document.getElementsByClassName("filter-drop-2");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
